529ee6c71dc826b430a7928a5236557648dd1dd9
ZhangHao0810 <874993952@qq.com>
2019-11-08 17:16:03 +0800

    环境配置,开头.

---
+++ store/src/utils/UUIDUtils.java
@@ -0,0 +1,33 @@
+package utils;
+
+import java.util.UUID;
+
+/** 2019/11/8 11:16
+ * 用于生成随机字符串.
+*/
+public class UUIDUtils {
+	/**
+	 * 随机生成id
+	 * @return
+	 */
+	public static String getId(){
+		return UUID.randomUUID().toString().replace("-", "").toUpperCase();
+	}
+	
+	
+	public static String getUUID64(){
+		return getId()+getId();
+	}
+	
+	/**
+	 * 生成随机码
+	 * @return
+	 */
+	public static String getCode(){
+		return getId();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getId());
+	}
+}
